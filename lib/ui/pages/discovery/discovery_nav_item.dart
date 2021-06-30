2d00b4c45ef65f0e49efd147c8e6e6f81d23028d
Jerry_sun <731238433@qq.com>
2021-06-30 15:59:46 +0800

    '发现页消息提示'

--- lib/ui/pages/discovery/discovery_nav_item.dart
+++ lib/ui/pages/discovery/discovery_nav_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wechat/core/extension/double_extension.dart';
+import 'package:flutter_wechat/ui/shared/app_theme.dart';
 
 class DiscoveryNavItem extends StatelessWidget {
   final String title;
@@ -32,6 +33,38 @@ class DiscoveryNavItem extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(title, style: TextStyle(fontSize: 44.0.px),),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(0, 0, 40.0.px, 0),
+                    alignment: Alignment.centerRight,
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(AppTheme.defaultBorderRadius * 0.4),
+                          child: Image.network(
+                            'https://oss-blog.myjerry.cn/avatar/blog-avatar.jpg',
+                            width: 86.0.px,
+                            height: 86.0.px,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Positioned(
+                          top: -15.0.px,
+                          right: -15.0.px,
+                          child: Container(
+                            height: 30.0.px,
+                            width: 30.0.px,
+                            decoration: BoxDecoration(
+                              color: Color(0xfffa5251),
+                              borderRadius: BorderRadius.circular(30.0.px),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
                 Icon(Icons.chevron_right, color: Color(0xffb2b2b2),)
               ],
             ),
