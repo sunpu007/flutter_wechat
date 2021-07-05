696ae3ccb8f7801989271054f1ecc1f94335f528
Jerry_sun <731238433@qq.com>
2021-07-05 16:22:38 +0800

    '抽离代码块'

--- lib/ui/pages/chat/chat.dart
+++ lib/ui/pages/chat/chat.dart
@@ -70,40 +70,7 @@ class _ChatPageState extends State<ChatPage> with WidgetsBindingObserver {
               },
             ),
           ),
-          Container(
-            constraints: BoxConstraints(
-              minHeight: 151.0.px
-            ),
-            padding: EdgeInsets.all(21.0.px),
-            decoration: BoxDecoration(
-              color: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
-            ),
-            child: Row(
-              children: [
-                Icon(Icons.keyboard_voice, size: 73.0.px,),
-                Expanded(
-                  child: Container(
-                    height: 105.0.px,
-                    margin: EdgeInsets.symmetric(horizontal: 29.0.px),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(AppTheme.defaultBorderRadius),
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Icon(Icons.emoji_emotions_outlined, size: 73.0.px,),
-                SizedBox(width: 35.0.px,),
-                Icon(Icons.add_circle_outline, size: 73.0.px,),
-              ],
-            ),
-          )
+          _buildBottomContext(context),
         ],
       ),
     );
@@ -179,6 +146,43 @@ class _ChatPageState extends State<ChatPage> with WidgetsBindingObserver {
               height: 108.0.px,
               fit: BoxFit.cover,),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomContext(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+          minHeight: 151.0.px
+      ),
+      padding: EdgeInsets.all(21.0.px),
+      decoration: BoxDecoration(
+        color: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.keyboard_voice, size: 73.0.px,),
+          Expanded(
+            child: Container(
+              height: 105.0.px,
+              margin: EdgeInsets.symmetric(horizontal: 29.0.px),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(AppTheme.defaultBorderRadius),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Icon(Icons.emoji_emotions_outlined, size: 73.0.px,),
+          SizedBox(width: 35.0.px,),
+          Icon(Icons.add_circle_outline, size: 73.0.px,),
         ],
       ),
     );
